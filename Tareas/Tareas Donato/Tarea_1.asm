a40b192b88806cbc543303aa12545c007e829938
Donato Lurgo <dalurgo10@gmail.com>
2026-08-16 20:35:24 -0300

    Tarea_1: version compacta pic-as (logica del profe, minimo e imprescindible, compila)

--- Tareas/Tareas Donato/Tarea_1.asm
+++ Tareas/Tareas Donato/Tarea_1.asm
@@ -1,107 +1,53 @@
-;===============================================================================
-; @file       APELLIDO_ED2_TAREA_1.asm
-;
-; @author     Lurgo Donato
-;
-; @date       13/8/2026
-;
-; @version    1.0
-;===============================================================================
+PROCESSOR 16F887
+#include <xc.inc>
 
-;===============================================================================
-; DIRECTIVAS DE INCLUSION
-;===============================================================================
-LIST P=16F887
-#include "p16f887.inc"
+CONFIG FOSC = INTRC_NOCLKOUT
+CONFIG WDTE = OFF
+CONFIG PWRTE = ON
+CONFIG MCLRE = ON
+CONFIG CP = OFF
+CONFIG CPD = OFF
+CONFIG BOREN = ON
+CONFIG IESO = OFF
+CONFIG FCMEN = OFF
+CONFIG LVP = OFF
+CONFIG BOR4V = BOR40V
+CONFIG WRT = OFF
 
-;===============================================================================
-; CONFIGURACION GENERAL DEL MCU
-;===============================================================================
-__CONFIG _CONFIG1, _FOSC_INTRC_NOCLKOUT & _WDTE_OFF & _PWRTE_ON & _MCLRE_ON & _CP_OFF & _CPD_OFF & _BOREN_ON & _IESO_OFF & _FCMEN_OFF & _LVP_OFF
-__CONFIG _CONFIG2, _BOR4V_BOR40V & _WRT_OFF
-
-;===============================================================================
-; DEFINICION DE CONSTANTES
-;===============================================================================
-; Retardo: cristal 4 MHz -> instruccion = 1 us -> 1 ms = 1000 ciclos
-; Cada pasada del bucle (NOP+NOP+DECFSZ+GOTO) = 5 ciclos
-; Contador = 200 -> 200 * 5 = 1000 ciclos = 1 ms
 TIEMPO  EQU     200
 
-;===============================================================================
-; DEFINICION DE VARIABLES
-;===============================================================================
-; Variable en RAM comun (0x70-0x7F), accesible sin cambiar de banco
-contador   EQU  0x70
+PSECT udata_shr,class=COMMON
+contador:
+    DS  1
 
-;===============================================================================
-; DECLARACION DE MACROS PARA CONFIGURACION DE REGISTROS
-;===============================================================================
+PSECT resetVec,class=CODE,delta=2
+resetVec:
+    GOTO    INICIO
 
-;===============================================================================
-; INICIALIZACION DEL MCU (CODIGO ABSOLUTO)
-;===============================================================================
-    ORG     0x00        ; Vector de Reset
-    GOTO    INICIO      ; Salto al inicio del programa principal
-
-    ORG     0x05        ; Ubicacion del programa principal
-
-;===============================================================================
-; INICIALIZACION DE MACROS PARA CONFIGURACION DE REGISTROS
-;===============================================================================
-INICIO
-    ;-----Inicializacion de Macros-------
-    ; Puerto B como salida
+PSECT code
+INICIO:
     BANKSEL TRISB
     CLRF    TRISB
-
     BANKSEL PORTB
     CLRF    PORTB
 
-;===============================================================================
-; INICIO PROGRAMA PRINCIPAL
-;===============================================================================
-MAIN_LOOP
-    ; Encender LEDs de Puerto B
+MAIN_LOOP:
     MOVLW   0xFF
     MOVWF   PORTB
-
-    ; Retardo de 1 ms
     CALL    retardo_1ms
-
-    ; Apagar LEDs de Puerto B
     BANKSEL PORTB
     CLRF    PORTB
-
-    ; Retardo de 1 ms
     CALL    retardo_1ms
-
     GOTO    MAIN_LOOP
 
-;===============================================================================
-; SUBRUTINAS
-;===============================================================================
-;*******************************************************************************
-; @brief    Retardo por software de 1 ms con 1 bucle.
-;
-; @details  Cristal 4 MHz -> clock de instruccion = FOSC/4 = 1 MHz
-;           -> 1 instruccion = 1 us
-;           -> 1 ms = 1000 us = 1000 ciclos de instruccion.
-;           Cada pasada del bucle (NOP + NOP + DECFSZ + GOTO) = 5 ciclos.
-;           Contador = 200 -> 200 * 5 = 1000 ciclos = 1 ms.
-;*******************************************************************************
-retardo_1ms
-    MOVLW   TIEMPO          ; contador = 200
+retardo_1ms:
+    MOVLW   TIEMPO
     MOVWF   contador
-
-bucle_retardo
+bucle_retardo:
     NOP
     NOP
-    DECFSZ  contador, F     ; decrementa; salta si llega a 0
+    DECFSZ  contador, F
     GOTO    bucle_retardo
-
     RETURN
 
-;===============================================================================
-    END
-;===============================================================================
+    END resetVec
