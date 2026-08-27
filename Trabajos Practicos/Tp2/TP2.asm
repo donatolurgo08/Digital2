c479acdccef3f602abce66224bd49236b2a4827e
smhopavc <smhopavc@users.noreply.github.com>
2026-08-27 18:22:36 -0300

    Actualizar TP2

--- Trabajos Practicos/Tp2/TP2.asm
+++ Trabajos Practicos/Tp2/TP2.asm
@@ -2,7 +2,7 @@
 ; @file       G6_TPL2_ED2.asm
 ;
 ; @author     Conde_Ana_Victoria
-;	          Lauc_Mirko_Gaston
+;	      Lauc_Mirko_Gaston
 ;             Bertalot_Gomez_Renata
 ;             Lurgo_Donato_Agustin
 ;             Goicoechea_Emilia
@@ -54,40 +54,40 @@ ENDC
 ; DECLARACION DE MACROS PARA CONFIGURACION DE REGISTROS
 ;===============================================================================
 CFG_SWITCH MACRO
-        BSF     STATUS,RP0  ;Switch presionado
-        BSF     STATUS,RP1  ; pasamos al banco 3 para configurar ansel
+        BSF     STATUS,RP0   ;Switch presionado
+        BSF     STATUS,RP1   ; pasamos al banco 3 para configurar ansel
         BCF     ANSEL,5
-        BCF     STATUS,RP1  ;ver q onda lo de los bancos
+        BCF     STATUS,RP1   ;ver q onda lo de los bancos
         BSF     TRISE,TRISE0 ;esto setea el pin RE0 como entrada
         BCF     STATUS,RP0   ;retorno de banco 0
 
 ENDM
 ;________________________________esto separa macros distintas pero dentro del bloque macro mamá________________________________________________
 CFG_LEDS MACRO
-        BSF     STATUS,RP0    ;Chequear estos puertos q no se q es
+        BSF     STATUS,RP0   ;Chequear estos puertos q no se q es
         BCF     STATUS,RP1
         CLRF    TRISD
         BCF     STATUS,RP0
-        CLRF    PORTD ; creo q aca chequeamos si el led pasa corriente, a estudiaar
+        CLRF    PORTD        ; creo q aca chequeamos si el led pasa corriente, a estudiaar
 
 ENDM
 ;________________________________________________________________________________
 ;------------------------estos guiones indican macros de la misma familia------------------------------------
 LEDS_ON MACRO
-        MOVLW  b'11111111' ;Chequear esto esta raaaro
+        MOVLW  b'11111111'   ;Chequear esto esta raaaro
         MOVWF   PORTD
 ENDM
 ;-------------------------------------------------------------------------------
 LEDS_OFF MACRO
-        CLRF   PORTD  ;aca se apagan todos los puertos
+        CLRF   PORTD         ;aca se apagan todos los puertos
 ENDM
 ;-------------------------------------------------------------------------------
 LEDS_RLF MACRO
-        RLF    PORTD,F  ;aca hacemos el desp a la izquirda de los leds, todos por eso se declara l puerto d
+        RLF    PORTD,F       ;aca hacemos el desp a la izquirda de los leds, todos por eso se declara l puerto d
 ENDM
 ;-------------------------------------------------------------------------------
 LEDS_RRF MACRO
-        RRF    PORTD,F  ;mover todo a la derecha ->
+        RRF    PORTD,F       ;mover todo a la derecha ->
 ENDM
 ;_______________________________________________________________________________
 CFG_BUZZER MACRO
@@ -226,14 +226,13 @@ LOOP3
 ; @details  Ejecuta un parpadeo sincronizado para los 8 LEDs, cumpliendo un
 ;           cumpliendo el periodo de itermitencia de 2 segundos.
 ;*******************************************************************************
-SUBROUTINE
 TEST_LEDS
         CFG_DELAY_1s
         LEDS_ON
         CALL    DELAY_3LOOP
         LEDS_OFF
         CALL    DELAY_3LOOP
-    RETURN
+RETURN
 ;*******************************************************************************
 ; @brief    Administacion de las diferentes secuencias de luces
 ;
@@ -242,24 +241,94 @@ TEST_LEDS
 ;           Forward y Backward) requeridos al presonar el pulsador.
 ;
 ;*******************************************************************************
-SUBROUTINE
         CALL    BUZZER_BIP
         CALL    RUNNING_LIGHT
         CALL    BIDIR_RUNNING_LIGHT
         CALL    CRAWLING
-        RETURN
+RETURN
 ;*******************************************************************************
 ; @brief    Alarma acustica  inicial
 ;
 ; @details  Enciende el buzzer conectado a RC0, ejecutando una demora de 200ms
 ;           y luego se apaga.
 ;*******************************************************************************
-SUBROUTINE
-        BUZZER_ON
+BUZZER_ON
         CFG_DELAY_200ms
-        CALL DELAY_3LOOP
+        CALL    DELAY_3LOOP
         BUZZER_OFF
-        RETURN
+RETURN
+
+;*******************************************************************************
+; @brief    Efecto de barrido (Running_Light)
+;
+; @details  ejecuta el barrido hacia la derecha (Forward_Led), repitiendo la secuencia
+;           tres veces (Counter_secuences) con una intermitencia de 300ms.
+;*******************************************************************************
+RUNNING_LIGHT
+        CFG_DELAY_300ms
+        LEDS_OFF
+LOOP_RL
+        CALL    FORWARD_LED
+        DECF    COUNTER_SECUENCES, F ; resta uno al contador de repeticiones
+        BTFSS   STATUS,Z  ; FLAG seteado(igual a 1)?
+        GOTO    LOOP_RL ; NO -> se repite el barrido
+        CFG_SECUENCES
+        ; SI -> el contador llego a 0, es decir, se repitió 3 veces
+
+RETURN
+
+;*******************************************************************************
+; @brief    Efecto de barrido bidereccional (Bidir_Running_Light)
+;
+; @details  ejecuta el barrido hacia la derecha (Forward_Led),despues hacia la
+;           izquierda(Backward_Led),  repitiendo la secuencia
+;           tres veces (Counter_secuences) con una intermitencia de 200ms.
+;*******************************************************************************
+BIDIR_RUNNING_LIGHT
+        CFG_DELAY_200ms
+        LEDS_OFF
+LOOP_BRL
+        CALL    FORWARD_LED
+        CALL    BACKWARD_LED
+        DECF    COUNTER_SECUENCES, F ; resta uno al contador de repeticiones
+        BTFSS   STATUS, Z  ; FLAG seteado(igual a 1)?
+        GOTO    LOOP_BRL ; NO -> se repite el barrido
+        CFG_SECUENCES
+        ; SI -> el contador llego a 0, es decir, se repitió 3 veces
+
+RETURN
+;*******************************************************************************
+; @brief    Encendido progresivo de los LEDs (Crawling ON).
+;
+; @details  Enciende los LEDs secuencialmente ingresando unos lógicos desde
+;           LED0 hacia LED7, manteniendo encendidos los anteriores hasta
+;           completar todo el puerto.
+;*******************************************************************************
+PROGRRESSIVE_LED_ON
+        BCF     STATUS, C       ;Carry limpio
+PROG_ON_LOOP
+        BSF     STATUS, C       ;Forzamos un '1' en el carry
+        LEDS_RLF
+        CALL    DELAY_3LOOP     ;Espera para visualizar el salto
+        BTFSS   PORTD,  7       ;Se encendio el ultimo led?
+        GOTO    PROG_ON_LOOP    ;NO -> repite el ciclo
+RETURN                          ;SI -> FIN
+;*******************************************************************************
+; @brief    Apagado progresivo de los 8 LEDs
+;
+; @details  Apaga los LEDs secuencialmente ingrsandoi ceros lógicos desde LED0
+;           hacia LED7
+;*******************************************************************************
+PROGRESSIVE_LED_OFF
+PROG_OFF_LOOP
+        BCF     STATUS, C       ; Forzamos un '0' en el carry para ir vaciando
+        LEDS_RLF
+        CALL    DELAY_3LOOP     ; Espera para visualizar el salto
+        MOVF    PORTD, F        ; Se mueve PORTD sobre sí mismo para actualizar el FLAG Z
+        BTFSS   STATUS, Z       ; El puerto D quedó en 0?
+        GOTO    PROG_OFF_LOOP   ; NO -> repite el ciclo
+        RETURN                  ; SI -> FIN
+
 ;===============================================================================
     END
 ;===============================================================================
