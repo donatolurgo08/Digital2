4db1980d08a411f28f844046ced13f97cc8e6f45
smhopavc <smhopavc@users.noreply.github.com>
2026-08-27 01:41:56 -0300

    Actualizar TP2

--- Trabajos Practicos/Tp2/TP2.asm
+++ Trabajos Practicos/Tp2/TP2.asm
@@ -27,16 +27,16 @@ __CONFIG _CONFIG1, _XT_OSC & _WDTE_OFF & _MCLRE_ON & _LVP_OFF
 ;===============================================================================
 ; DEFINICION DE CONSTANTES
 ;===============================================================================
-#DEFINE SW1   PORTA,RA4
-#DEFINE BUZZ  PORTB,RC0
-#DEFINE LED0  PORTC,RD0
-#DEFINE LED1  PORTC,RD1
-#DEFINE LED2  PORTC,RD2
-#DEFINE LED3  PORTC,RD3
-#DEFINE LED4  PORTC,RD4
-#DEFINE LED5  PORTC,RD5
-#DEFINE LED6  PORTC,RD6
-#DEFINE LED7  PORTC,RD7
+#DEFINE SW1   PORTE,RE0
+#DEFINE BUZZ  PORTC,RC0
+#DEFINE LED0  PORTD,RD0
+#DEFINE LED1  PORTD,RD1
+#DEFINE LED2  PORTD,RD2
+#DEFINE LED3  PORTD,RD3
+#DEFINE LED4  PORTD,RD4
+#DEFINE LED5  PORTD,RD5
+#DEFINE LED6  PORTD,RD6
+#DEFINE LED7  PORTD,RD7
 ;===============================================================================
 ; DEFINICION DE VARIABLES
 ;===============================================================================
@@ -44,48 +44,38 @@ CBLOCK 0x20
          DELAY1_Init
          DELAY2_Init
          DELAY3_Init
+         DELAY1
+         DELAY2
+         DELAY3
          COUNTER_LED
          COUNTER_SECUENCES
+ENDC
 ;===============================================================================
 ; DECLARACION DE MACROS PARA CONFIGURACION DE REGISTROS
 ;===============================================================================
 CFG_SWITCH MACRO
         BSF     STATUS,RP0  ;Switch presionado
-        BCF     STATUS,RP1  ;Switch no presionado
-        BSF     TRISA,TRISA4 ;RA4 entrada digital ver que carajo es el tris
+        BSF     STATUS,RP1  ; pasamos al banco 3 para configurar ansel
+        BCF     ANSEL,5
+        BCF     STATUS,RP1  ;ver q onda lo de los bancos
+        BSF     TRISE,TRISE0 ;esto setea el pin RE0 como entrada
+        BCF     STATUS,RP0   ;retorno de banco 0
 
 ENDM
 ;________________________________esto separa macros distintas pero dentro del bloque macro mamá________________________________________________
 CFG_LEDS MACRO
         BSF     STATUS,RP0    ;Chequear estos puertos q no se q es
-        BSF     STATUS,RP1
-        BCF     ANSELH,RD0    ;ver que es el anselh xd
-        BCF     ANSELH,RD1    ; y como es la distribucion de los bancos
-        BCF     ANSELH,RD2
-        BCF     ANSELH,RD3
-        BCF     ANSELH,RD4
-        BCF     ANSELH,RD5
-        BCF     ANSELH,RD6
-        BCF     ANSELH,RD7
-        BCF     STATUS,RP1    ;fijate si esta en cero basicamente, supuestamente termina el banco aca
-        CLRF     TRISD ; creo q aca chequeamos si el led pasa corriente, a estudiaar
-        BCF     STATUS,RP0  ;verr por que se eligen el rp1,rp0 etcetc
-        BCF     LED0
-        BCF     LED1
-        BCF     LED2
-        BCF     LED3
-        BCF     LED4
-        BCF     LED5
-        BCF     LED6
-        BCF     LED7
-        ; VER POR QUE NO SE CIERRA LA FUNCION, COMO QUE EL  PROGRAMA ESPERA ALGO??? capaz sea por el VISUAL code
-        ;Se supone que los leds estan apagados por el bcf
-        ENDM
+        BCF     STATUS,RP1
+        CLRF    TRISD
+        BCF     STATUS,RP0
+        CLRF    PORTD ; creo q aca chequeamos si el led pasa corriente, a estudiaar
+
+ENDM
 ;________________________________________________________________________________
 ;------------------------estos guiones indican macros de la misma familia------------------------------------
 LEDS_ON MACRO
         MOVLW  b'11111111' ;Chequear esto esta raaaro
-        MOWF   PORTD
+        MOVWF   PORTD
 ENDM
 ;-------------------------------------------------------------------------------
 LEDS_OFF MACRO
@@ -159,7 +149,9 @@ CFG_DELAY_1s MACRO
         MOVWF DELAY3_Init   ; calculamos siempre con la formula de arriba
 ;_______________________________________________________________________________
 CFG_SECUENCES MACRO
-
+        MOVLW D'3'
+        MOVWF COUNTER_SECUENCES
+ENDM
 ;===============================================================================
 ; INICIALIZACION DEL MCU (CODIGO ABSOLUTO)
 ;===============================================================================
@@ -172,27 +164,72 @@ CFG_SECUENCES MACRO
 ; INICIALIZACION DE MACROS PARA CONFIGURACION DE REGISTROS
 ;===============================================================================
 INICIO	    ;-----Inicializacion de Macros-------
-
+        CFG_SWITCH
+        ;↑ configura el pin del pulsador
+        CFG_LEDS
+        ;↑ Configura el puerto de los LEDs
+        CFG_SECUENCES
+        ;↑ Carga el contador de repeticiones en 3
+        CFG_BUZZER
+        ;↑ Configura el pin del buzzer
+        BUZZER_OFF
+        ;↑ Garantice que el buzzer incie desactivado
 
 ;===============================================================================
 ; INICIO PROGRAMA PRINCIPAL
 ;===============================================================================
 MAIN_LOOP
-    ;...
+        CALL    TEST_LEDS
+        ;**** preguntamos si el switch esta presionado o no ****
+        BTFSC   SW1
     GOTO    MAIN_LOOP
-
+        ;*** Si se skipeo la linea 185, llamamos a las secuencias
+        ;segun el diagrama de flujo
+        CALL    CFG_SECUENCES
+    GOTO    MAIN_LOOP
 ;===============================================================================
 ; SUBRUTINAS
 ;===============================================================================
 ;*******************************************************************************
-; @brief    Descripcion general de la subrutina.
+; @brief    Testeo de hardware para los 8 LEDs
 ;
-; @details  Descripcion especifica de la subrutina.
+; @details  Ejecuta un parpadeo sincronizado para los 8 LEDs, cumpliendo un
+;           cumpliendo el periodo de itermitencia de 2 segundos.
 ;*******************************************************************************
 SUBROUTINE
-    ;...
+TEST_LEDS
+        CFG_DELAY_1s
+        LEDS_ON
+        CALL    DELAY_3LOOP
+        LEDS_OFF
+        CALL    DELAY_3LOOP
     RETURN
-
+;*******************************************************************************
+; @brief    Administacion de las diferentes secuencias de luces
+;
+; @details  Agrupa y ejecuta de forma secuencias la alarma acustica inicial
+;           y los tres efectos visuales (Running, Bidir-Running, Crawling,
+;           Forward y Backward) requeridos al presonar el pulsador.
+;
+;*******************************************************************************
+SUBROUTINE
+        CALL    BUZZER_BIP
+        CALL    RUNNING_LIGHT
+        CALL    BIDIR_RUNNING_LIGHT
+        CALL    CRAWLING
+        RETURN
+;*******************************************************************************
+; @brief    Alarma acustica  inicial
+;
+; @details  Enciende el buzzer conectado a RC0, ejecutando una demora de 200ms
+;           y luego se apaga.
+;*******************************************************************************
+SUBROUTINE
+        BUZZER_ON
+        CFG_DELAY_200ms
+        CALL DELAY_3LOOP
+        BUZZER_OFF
+        RETURN
 ;===============================================================================
     END
 ;===============================================================================
